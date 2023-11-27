28bb2fcd5cdc934380c471627c1a0ba76ab60f72
SuperNova <nariman.bakhyt.06@mail.ru>
2023-11-27 18:23:34 +0600

    класс oil

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
+
+class oil {
+public:
+	int age;
+	std::string name;
+};
 
 int main(){
-	std::cout << " privet " << std::endl;
+	 
+	oil MI;
+	MI.age = 19;
+	MI.name = "Нариман";
 
+
+	std::cout << MI.age << "\n" << MI.name << std::endl;
+	return 0;
 
 }
